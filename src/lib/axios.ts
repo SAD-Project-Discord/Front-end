5e65b72cb6b8fb0ecdc45ed2657baa1d90756730
Mahdi Abedini <m.abedini30@yahoo.com>
2026-07-05 21:35:13 +0330

    chore: configure axios

---
+++ src/lib/axios.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    // For attaching auth token later.
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
